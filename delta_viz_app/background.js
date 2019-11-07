ee906628c78bf8b01e59fb896f7ab35d206ed9f8
Heethesh Vhavle <heethesh@gmail.com>
2019-11-07 05:27:43 -0500

    Update app size

--- delta_viz_app/background.js
+++ delta_viz_app/background.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     id: 'main',
-    bounds: { width: 1280, height: 720 }
+    bounds: { width: 1600, height: 975 }
   });
 });
