32ef6a79d787b1f439b07e18c447b1ee7fa71883
Heethesh Vhavle <heethesh@gmail.com>
2019-11-07 05:18:05 -0500

    Make index.html main

--- delta_viz_app/background.js
+++ delta_viz_app/background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('app.html', {
+  chrome.app.window.create('index.html', {
     id: 'main',
     bounds: { width: 1280, height: 720 }
   });
